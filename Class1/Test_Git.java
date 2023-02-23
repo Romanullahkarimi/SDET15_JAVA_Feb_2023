40348f8b42a87f275d1dc5758301a24fd69d1918
Romanullahkarimi <104285744+Romanullahkarimi@users.noreply.github.com>
2023-02-23 11:49:23 -0500

    message

--- Class1/Test_Git.java
+++ Class1/Test_Git.java
@@ -3,5 +3,6 @@ package Class1;
 public class Test_Git {
     public static  void main(String args[]){
         System.out.println("Hello Git Hub");
+        System.out.println("first class");
     }
 }
